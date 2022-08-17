d9736228931eadac7e2a96130418509f4552c9e0
Александр <westerovs@mail.ru>
2022-08-17 22:33:03 +0300

    24 shaders complete

--- src/24-shaders/src/main.js
+++ src/24-shaders/src/main.js
@@ -1,100 +1,117 @@
 import * as THREE from 'three'
+import * as dat from 'dat.gui'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-// import testVertexShader from './shaders/vertex.glsl'
-import testFragmentShader from './shaders/vertex.glsl'
-//
-/* eslint-disable */
-// constants
+import testVertexShader from './shaders/vertex.glsl'
+import testFragmentShader from './shaders/fragment.glsl'
+
+
+/**
+ * Base
+ */
+  // Debug
+const gui = new dat.GUI()
+
+// Canvas
 const canvas = document.querySelector('canvas.webgl')
+
+// Scene
 const scene = new THREE.Scene()
+
+/**
+ * Textures
+ */
+const textureLoader = new THREE.TextureLoader()
+const flagTexture   = textureLoader.load('./static/cover.jpg')
+
+/**
+ * Test mesh
+ */
+      // Geometry
+const geometry = new THREE.PlaneBufferGeometry(1, 1, 32, 32)
+
+const count   = geometry.attributes.position.count
+const randoms = new Float32Array(count)
+
+for (let i = 0; i < count; i++) {
+  randoms[i] = Math.random()
+}
+
+geometry.setAttribute('aRandom', new THREE.BufferAttribute(randoms, 1))
+
+// Material
+const material = new THREE.ShaderMaterial({
+  vertexShader  : testVertexShader,
+  fragmentShader: testFragmentShader,
+  uniforms      :
+    {
+      uFrequency: { value: new THREE.Vector2(10, 5) },
+      uTime     : { value: 0 },
+      uColor    : { value: new THREE.Color('orange') },
+      uTexture  : { value: flagTexture },
+    },
+})
+
+gui.add(material.uniforms.uFrequency.value, 'x').min(0).max(20).step(0.01).name('frequencyX')
+gui.add(material.uniforms.uFrequency.value, 'y').min(0).max(20).step(0.01).name('frequencyY')
+
+// Mesh
+const mesh   = new THREE.Mesh(geometry, material)
+mesh.scale.y = 2 / 3
+scene.add(mesh)
+
+/**
+ * Sizes
+ */
 const sizes = {
-  width: window.innerWidth,
+  width : window.innerWidth,
   height: window.innerHeight,
 }
 
-const geometry = new THREE.PlaneGeometry(5, 5, 32, 32)
+window.addEventListener('resize', () => {
+  // Update sizes
+  sizes.width  = window.innerWidth
+  sizes.height = window.innerHeight
 
-const material = new THREE.RawShaderMaterial({
-  vertexShader  : `
-      uniform mat4 projectionMatrix;
-      uniform mat4 viewMatrix;
-      uniform mat4 modelMatrix;
+  // Update camera
+  camera.aspect = sizes.width / sizes.height
+  camera.updateProjectionMatrix()
 
-      attribute vec3 position;
-
-      void main() {
-        gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
-      }
-    `,
-  fragmentShader: `
-      precision mediump float;
-
-      void main() {
-        gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
-      }
-  `
-})
-
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
-
-// ======================= Textures =====================
-
-const createCube = () => {
-  const geometry = new THREE.BoxGeometry()
-  const material = new THREE.MeshBasicMaterial({ color: 0xcc4400 })
-
-  return  new THREE.Mesh(geometry, material)
-}
-
-const cube = createCube()
-// scene.add(cube)
-
-// ======================= Camera =======================
-// Base camera
-const createCamera = () => {
-  const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-  camera.position.x = 4
-  camera.position.y = 2
-  camera.position.z = 5
-  scene.add(camera)
-
-  return camera
-}
-const camera = createCamera()
-
-
-// ======================= Controls =====================
-const createControls = () => {
-  const controls = new OrbitControls(camera, canvas)
-  controls.enableDamping = true
-
-  return controls
-}
-const controls = createControls()
-
-
-// ======================= Renderer =====================
-const createRenderer = () => {
-  const renderer = new THREE.WebGLRenderer({
-    canvas: canvas,
-  })
-  renderer.shadowMap.enabled = true
-  renderer.shadowMap.type = THREE.PCFSoftShadowMap
-  renderer.setClearColor('#262837')
+  // Update renderer
   renderer.setSize(sizes.width, sizes.height)
   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
 
-  return renderer
-}
-const renderer = createRenderer()
+/**
+ * Camera
+ */
+      // Base camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.set(0.25, -0.25, 1)
+scene.add(camera)
 
+// Controls
+const controls         = new OrbitControls(camera, canvas)
+controls.enableDamping = true
 
-// ======================= Tick =========================
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+/**
+ * Animate
+ */
 const clock = new THREE.Clock()
 
 const tick = () => {
   const elapsedTime = clock.getElapsedTime()
+
+  // Update material
+  material.uniforms.uTime.value = elapsedTime
 
   // Update controls
   controls.update()
@@ -106,26 +123,4 @@ const tick = () => {
   window.requestAnimationFrame(tick)
 }
 
-// ======================= resize =======================
-const initSignals = () => {
-  window.addEventListener('resize', () => {
-    // Update sizes
-    sizes.width  = window.innerWidth
-    sizes.height = window.innerHeight
-
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
-
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-  })
-}
-initSignals()
-
-const initGame = () => {
-  tick()
-}
-
-initGame()
+tick()
